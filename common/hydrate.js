17d0298671062cd0b256e5c3931af9a0190c8c4b
Alistair Brown <github@alistairjcbrown.com>
2024-10-31 19:04:35 +0000

    Improve hydration matching
    Extract out phrases to remove from titles, and fix year increment

--- common/hydrate.js
+++ common/hydrate.js
@@ -1,6 +1,7 @@
 const slugify = require("slugify");
 const { MovieDb } = require("moviedb-promise");
 const { dailyCache } = require("./cache");
+const knownRemovablePhrases = require("./known-removable-phrases.json");
 require("dotenv").config();
 
 const moviedb = new MovieDb(process.env.MOVIEDB_API_KEY);
@@ -8,13 +9,13 @@ const moviedb = new MovieDb(process.env.MOVIEDB_API_KEY);
 const getAndCacheData = ({ slug, year, normalizedTitle }) =>
   dailyCache(`moviedb-${slug}`, async () => {
     const payload = { query: normalizedTitle };
-    if (year) payload.year = year;
+    if (year) payload.year = parseInt(year, 10);
     const search = await moviedb.searchMovie(payload);
 
     // If there's a year available, sometimes the movie listing
     // the year off by 1. Let's try again with the year incremented.
     if (search.results.length === 0 && year) {
-      payload.year = year + 1;
+      payload.year = parseInt(year, 10) + 1;
       return moviedb.searchMovie(payload);
     }
 
@@ -49,80 +50,6 @@ function normalize(title) {
     title = hasBrackets[1];
   }
 
-  const knownRemovablePhrases = [
-    ": Double Feature",
-    "Sing-A-Long-A",
-    "Re-release",
-    "Rerelease",
-    ": Director's Definitive Edition",
-    "4K Restoration",
-    ": The Final Cut",
-    ": Final Cut",
-    "Theatrical Cut",
-    "Director's Cut",
-    "40th Anniversar",
-    "PINK PALACE:",
-    "Classic Matinee:",
-    ": Family Flicks Hallowe'en Special",
-    "Bar Trash:",
-    "Staff Selects Halloween Special:",
-    "Carers & Babies:",
-    "Carers & Babies Club:",
-    "DOC'N ROLL:",
-    "Doc'N Roll FF 24:",
-    "Doc'n Roll Film Festival Presents:",
-    "for Halloween",
-    "- UK PREMIERE",
-    ": ANI DiFRANCO",
-    "- Filum Film Club",
-    "Scared To Dance -",
-    "FILM CLUB:",
-    "Call To Action:",
-    "Carers and Babies Club:",
-    "Film Africa:",
-    "The Dead Carpet :",
-    "ATOMIC ORIGINS:",
-    "Exhibition On Screen:",
-    "Green Screen:",
-    "SLA:",
-    "OUT at Clapham:",
-    "OUT:",
-    "UK Premiere:",
-    "London Korean Film Festival Opening Gala:",
-    "London Korean Film Festival Closing Night Gala:",
-    "Closing Night Gala:",
-    "60th anniversary screenings",
-    "60th anniversary screening",
-    "with intro",
-    "with Dr Catherine Lester",
-    "with Tim Robey",
-    "(BFI Classics) with David Forrest",
-    "Library Talks Screening:",
-    "Funday:",
-    "Halloween special:",
-    "Preview:",
-    "Preview screening:",
-    "LSF 2025:",
-    "Seniors’ Free Talk:",
-    "Seniors’ Free Matinee:",
-    "Seniors’ Matinee:",
-    "Relaxed screening:",
-    "Member Mondays:",
-    "Member Picks:",
-    "Member screening:",
-    "Member Exclusive:",
-    "Art of Action:",
-    "Opening Night:",
-    "Library Talk:",
-    "New Writings:",
-    "Closing Night:",
-    "28th MADE IN PRAGUE FESTIVAL:",
-    "28th MADE IN PRAGE FESTIVAL GALA:",
-    "CinemaItaliaUK Special Screening of",
-    ": SCRT X MUBI PRESENT",
-    "London Breeze Festival:",
-    "1999 Season:",
-  ];
   knownRemovablePhrases.forEach((phrase) => {
     title = title.replace(phrase.toLowerCase(), "");
   });
