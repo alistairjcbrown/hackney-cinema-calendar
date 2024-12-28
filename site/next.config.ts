93bad231f5a80965ae5538ee877ec0df933d67d5
Alistair Brown <github@alistairjcbrown.com>
2024-12-28 17:26:32 +0100

    Align next and github pages trailing slash to prevent redirects

--- site/next.config.ts
+++ site/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   basePath: "/hackney-cinema-calendar",
+  trailingSlash: true,
   output: "export",
   reactStrictMode: true,
   sassOptions: {
