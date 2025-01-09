a2d2c5dfcdc54353a2edbf2bcc56bc79b4589c9b
Alistair Brown <github@alistairjcbrown.com>
2025-01-09 23:25:53 +0000

    Update github action to try and get vue tasks to run

--- common/myvue.com/retrieve.js
+++ common/myvue.com/retrieve.js
@@ -4,6 +4,7 @@ async function retrieve({ domain, url, cinemaId }) {
   const cacheKey = `myvue.com-${cinemaId}`;
   return await getPageWithPlaywright(url, cacheKey, async (page) => {
     await page.waitForLoadState();
+    await page.locator(".cinema-info-links").waitFor();
     return page.evaluate(
       (url) => fetch(url).then((response) => response.json()),
       `${domain}/api/microservice/showings/cinemas/${cinemaId}/films`,
