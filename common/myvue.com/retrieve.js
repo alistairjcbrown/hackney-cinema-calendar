295b0719cfd64d6d1fc3ba429f52df45107fe04b
Alistair Brown <github@alistairjcbrown.com>
2025-01-10 00:20:56 +0000

    Try out zenrows for accessing vue pages

--- common/myvue.com/retrieve.js
+++ common/myvue.com/retrieve.js
@@ -1,15 +1,24 @@
-const getPageWithPlaywright = require("../get-page-with-playwright");
+const { chromium } = require("playwright-extra");
+
+require("dotenv").config();
+
+const stealth = require("puppeteer-extra-plugin-stealth")();
+chromium.use(stealth);
+
+const connectionURL = `wss://browser.zenrows.com?apikey=${process.env.ZENROWS_API_KEY}&proxy_country=gb`;
 
 async function retrieve({ domain, url, cinemaId }) {
-  const cacheKey = `myvue.com-${cinemaId}`;
-  return await getPageWithPlaywright(url, cacheKey, async (page) => {
-    await page.waitForLoadState();
-    await page.locator(".cinema-info-links").waitFor();
-    return page.evaluate(
-      (url) => fetch(url).then((response) => response.json()),
-      `${domain}/api/microservice/showings/cinemas/${cinemaId}/films`,
-    );
-  });
+  const browser = await chromium.connectOverCDP(connectionURL);
+  const page = await browser.newPage();
+  await page.goto(url);
+  await page.waitForLoadState();
+  await page.locator(".header__box").waitFor();
+  const result = await page.evaluate(
+    (url) => fetch(url).then((response) => response.json()),
+    `${domain}/api/microservice/showings/cinemas/${cinemaId}/films`,
+  );
+  await browser.close();
+  return result;
 }
 
 module.exports = retrieve;
