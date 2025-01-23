bea860a02835af0a8366a47a2c7730583223d73c
Alistair Brown <github@alistairjcbrown.com>
2025-01-23 18:18:36 +0000

    Add tests for Curzon

---
+++ cinemas/curzon.com-soho/test/index.test.js
@@ -0,0 +1,31 @@
+/** @jest-environment setup-polly-jest/jest-environment-node */
+const { setupPolly, schemaValidate } = require("../../../common/test-utils");
+const { retrieve, transform, attributes } = require("..");
+
+const isRecording = false;
+
+describe(attributes.name, () => {
+  setupPolly(isRecording, __dirname);
+  jest.useFakeTimers().setSystemTime(new Date("2025-01-23"));
+
+  it(
+    "retrieve and transform",
+    async () => {
+      const moviePages = await retrieve();
+
+      // Make sure the input looks roughly correct
+      expect(moviePages).toBeTruthy();
+      expect(moviePages.length).toBe(16);
+
+      const output = await transform(moviePages, {});
+      const data = JSON.parse(JSON.stringify(output));
+
+      // Make sure the data looks roughly correct
+      expect(data.length).toBe(17); // This count is not related to the above
+
+      expect(schemaValidate(data)).toBe(true);
+      expect(data).toMatchSnapshot();
+    },
+    isRecording ? 60_000 : undefined,
+  );
+});
