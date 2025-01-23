5f2b97177b0fb544bf157d7a0522a56c0ad82940
Alistair Brown <github@alistairjcbrown.com>
2025-01-23 17:29:29 +0000

    Add test for BFI IMAX with manual recording data

---
+++ cinemas/bfi.org.uk-imax/tests/index.test.js
@@ -0,0 +1,38 @@
+const path = require("node:path");
+const { schemaValidate } = require("../../../common/test-utils");
+const { dailyCache } = require("../../../common/cache");
+const { readCache } = jest.requireActual("../../../common/cache");
+const { retrieve, transform, attributes } = require("..");
+
+// Mock out the caching function so we can return manual cache recordings for
+// repsonses without needing to spin up a full browser
+jest.mock("../../../common/cache");
+dailyCache.mockImplementation((key) =>
+  readCache(key, (filename) =>
+    path.join(__dirname, "__manual-recordings__", `${filename}-2025-01-23`),
+  ),
+);
+
+// Hide script output
+console.log = () => {};
+
+describe(attributes.name, () => {
+  jest.useFakeTimers().setSystemTime(new Date("2025-01-23"));
+
+  it("retrieve and transform", async () => {
+    const moviePages = await retrieve();
+
+    // Make sure the input looks roughly correct
+    expect(moviePages).toBeTruthy();
+    expect(Object.keys(moviePages).length).toBe(39);
+
+    const output = await transform(moviePages, {});
+    const data = JSON.parse(JSON.stringify(output));
+
+    // Make sure the data looks roughly correct
+    expect(data.length).toBe(39);
+
+    expect(schemaValidate(data)).toBe(true);
+    expect(data).toMatchSnapshot();
+  });
+});
