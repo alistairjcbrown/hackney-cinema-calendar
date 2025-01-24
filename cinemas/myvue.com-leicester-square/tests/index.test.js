018284c2a56f7a66e4fb454cb931cc686f92f27e
Alistair Brown <github@alistairjcbrown.com>
2025-01-24 01:08:18 +0000

    Add tests for Vue

---
+++ cinemas/myvue.com-leicester-square/tests/index.test.js
@@ -0,0 +1,29 @@
+const {
+  setupCacheMock,
+  schemaValidate,
+} = require("../../../common/test-utils");
+const { retrieve, transform, attributes } = require("..");
+
+jest.mock("../../../common/cache");
+setupCacheMock(__dirname, "2025-01-24");
+
+describe(attributes.name, () => {
+  jest.useFakeTimers().setSystemTime(new Date("2025-01-24"));
+
+  it("retrieve and transform", async () => {
+    const moviePages = await retrieve();
+
+    // Make sure the input looks roughly correct
+    expect(moviePages).toBeTruthy();
+    expect(moviePages.result.length).toBe(37);
+
+    const output = await transform(moviePages, {});
+    const data = JSON.parse(JSON.stringify(output));
+
+    // Make sure the data looks roughly correct
+    expect(data.length).toBe(37);
+
+    expect(schemaValidate(data)).toBe(true);
+    expect(data).toMatchSnapshot();
+  });
+});
